decb5deec0ac22a2435892ee13745953e8367042
bgarbitt <bgarbitt@ualberta.ca>
2017-02-27 15:52:58 -0700

    pre-merge

---
+++ app/src/androidTest/java/ca/ualberta/andromeda/UserTest.java
@@ -0,0 +1,10 @@
+package ca.ualberta.andromeda;
+
+import org.junit.Test;
+import java.util.Date;
+import static org.junit.Assert.*;
+
+/**
+ * Created by brettgarbitt on 2017-02-27.
+ */
+
--- app/src/main/java/ca/ualberta/andromeda/User.java
+++ app/src/main/java/ca/ualberta/andromeda/User.java
@@ -1,7 +1,44 @@
 package ca.ualberta.andromeda;
 
+import java.util.ArrayList;
+
 /**
  * Created by pensk on 2017/02/27.
  */
 public class User {
+
+    private String Username;
+    private String Password;
+    private ArrayList<String> Following;
+    private ArrayList<String> FollowerRequest;
+
+
+    public User(String Username, String Password,
+                ArrayList<String> Following,
+                ArrayList<String> FollowerRequest) {
+        this.Username = Username;
+        this.Password = Password;
+        this.Following = Following;
+        this.FollowerRequest = FollowerRequest;
+    }
+
+    public ArrayList<String> getFollowingList() { return this.Following; }
+
+    public ArrayList<String> getFollowRequest() { return this.FollowerRequest;}
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
 }

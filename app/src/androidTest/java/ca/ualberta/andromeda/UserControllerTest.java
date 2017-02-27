5f7617461e1811f2d67feb749c6dd78eb32c4c2e
Pensk <pensk@hotmail.ca>
2017-02-27 15:27:47 -0700

    user controller test

---
+++ app/src/androidTest/java/ca/ualberta/andromeda/UserControllerTest.java
@@ -0,0 +1,26 @@
+package ca.ualberta.andromeda;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by pensk on 2017/02/27.
+ */
+public class UserControllerTest {
+    @Test
+    public void testAddUser() {
+
+    }
+
+    @Test
+    public void testGetUser() {
+
+    }
+
+    @Test
+    public void testDeleteUser() {
+
+    }
+
+}

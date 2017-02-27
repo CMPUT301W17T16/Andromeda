53075ab24d8144749ff4b799eeeadf5a4df923ab
bgarbitt <bgarbitt@ualberta.ca>
2017-02-27 13:35:50 -0700

    Attempting to pull from git

---
+++ app/src/main/java/ca/ualberta/andromeda/Notifications.java
@@ -0,0 +1,27 @@
+package ca.ualberta.andromeda;
+
+import java.util.ArrayList;
+
+/**
+ * Created by brettgarbitt on 2017-02-27.
+ */
+
+public class Notifications {
+    private ArrayList<String> requests;
+
+    public void viewUser() {
+
+    }
+
+    public void addToFollowingList() {
+
+    }
+
+    public void getRequests() {
+
+    }
+
+    public void removeFollowingList() {
+        
+    }
+}

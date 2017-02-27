01cad65730b4c9406044fdeb3326215b56db8859
Pensk <pensk@hotmail.ca>
2017-02-27 14:53:54 -0700

    user class attributes

--- app/src/main/java/ca/ualberta/andromeda/User.java
+++ app/src/main/java/ca/ualberta/andromeda/User.java
@@ -1,7 +1,25 @@
 package ca.ualberta.andromeda;
 
+import java.util.ArrayList;
+
 /**
  * Created by pensk on 2017/02/27.
  */
 public class User {
+    private String username;
+    private String password;
+    private ArrayList<User> following;
+    private ArrayList<User> followerRequest;
+
+    public User(){
+
+    }
+
+    public ArrayList<User> getFollowing(){
+        return this.following;
+    }
+
+    public ArrayList<User> getFollowRequest(){
+
+    }
 }

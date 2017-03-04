6058236d4d1e33410d5e80c0c30d5b03c09aee74
Pensk <pensk@hotmail.ca>
2017-03-04 12:55:28 -0700

    MoodModel interfaces model

--- app/src/main/java/ca/ualberta/andromeda/MoodModel.java
+++ app/src/main/java/ca/ualberta/andromeda/MoodModel.java
@@ -6,23 +6,42 @@ import java.util.ArrayList;
  * Created by pensk on 2017/02/27.
  */
 
-public class MoodModel {
+public class MoodModel implements Model<Mood> {
     private ArrayList<Mood> moodList;
 
     public MoodModel(){
-        this.moodList = this.loadMoods();
+        this.moodList = this.loadList();
     }
 
-    public ArrayList<Mood> getMoods(){
+    @Override
+    public ArrayList<Mood> getList() {
         return this.moodList;
     }
 
-    public ArrayList<Mood> loadMoods(){
-        /* load from disk */
-        return new ArrayList<Mood>();
+    @Override
+    public Mood getItem(int index) {
+        return this.moodList.get(index);
     }
 
-    public void saveMoods(){
+    @Override
+    public void deleteItem(int index) {
+        this.moodList.remove(index);
+        this.saveList();
+    }
+
+    @Override
+    public void addItem(Mood mood) {
+        this.moodList.add(mood);
+        this.saveList();
+    }
+
+    @Override
+    public ArrayList<Mood> loadList() {
+        return this.moodList;
+    }
+
+    @Override
+    public void saveList() {
         /* save to disk */
     }
 }

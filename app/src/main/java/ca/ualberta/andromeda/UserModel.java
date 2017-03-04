96936eef6a1ff8e1ac3192970f1c333ef0fbd4d4
Pensk <pensk@hotmail.ca>
2017-03-04 12:55:48 -0700

    UserModel interfaces Model

--- app/src/main/java/ca/ualberta/andromeda/UserModel.java
+++ app/src/main/java/ca/ualberta/andromeda/UserModel.java
@@ -5,23 +5,37 @@ import java.util.ArrayList;
 /**
  * Created by pensk on 2017/02/27.
  */
-public class UserModel {
+public class UserModel implements Model<User> {
     private ArrayList<User> userList;
 
     public UserModel(){
-        this.userList = this.loadUsers();
+        this.userList = this.loadList();
     }
 
-    public ArrayList<User> getUsers(){
+    public ArrayList<User> getList(){
         return this.userList;
     }
 
-    public ArrayList<User> loadUsers(){
+    public User getItem(int index){
+        return this.userList.get(index);
+    }
+
+    public void addItem(User user){
+        this.userList.add(user);
+        this.saveList();
+    }
+
+    public void deleteItem(int index){
+        this.userList.remove(index);
+        this.saveList();
+    }
+
+    public ArrayList<User> loadList(){
         /* load from disk */
         return new ArrayList<User>();
     }
 
-    public void saveUsers(){
+    public void saveList(){
         /* save to disk */
     }
 }

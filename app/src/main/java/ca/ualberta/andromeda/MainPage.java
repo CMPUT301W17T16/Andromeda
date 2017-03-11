7d06332e0f3416a959078032f0b8bb83a56ecd39
brianzbw <brian.zbw@gmail.com>
2017-03-11 14:24:59 -0700

    set login link to main page

--- app/src/main/java/ca/ualberta/andromeda/MainPage.java
+++ app/src/main/java/ca/ualberta/andromeda/MainPage.java
@@ -17,29 +17,42 @@ public class MainPage extends AppCompatActivity {
     private ListView oldMoodList;
     private ArrayList<Mood> moodList = new ArrayList<Mood>();
     private ArrayAdapter<Mood> adapter;
+    User user;
+    String username;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+        oldMoodList = (ListView) findViewById(R.id.MoodList);
 
         Button myMoodsButton = (Button) findViewById(R.id.button);
         Button mapButton = (Button) findViewById(R.id.button3);
         Button filterButton = (Button) findViewById(R.id.button2);
+//
+//        mapButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Intent intent = new Intent(v.getContext(), Map.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//
+//        });
+//
+//        filterButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                Intent intent = new Intent(v.getContext(), Filter.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//
+//        });
 
-        mapButton.setOnClickListener(new View.OnClickListener() {
+        myMoodsButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), Map.class);
-                startActivity(intent);
-                finish();
-            }
-
-        });
-
-        filterButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), Filter.class);
+                Intent intent = new Intent(v.getContext(),UsersMoods.class);
+                intent.putExtra("user", username);
                 startActivity(intent);
                 finish();
             }
@@ -56,12 +69,13 @@ public class MainPage extends AppCompatActivity {
     protected void onStart() {
 
         super.onStart();
-
-        // TODO:need getMoodList in the moodController 2017/3/5
-       // moodList = MoodController.getModdList();
+        Intent intent = getIntent();
+        username = intent.getStringExtra("user");
+        //// TODO:need getMoodList in the moodController 2017/3/5
+        //moodList = MoodController.getModdList();
 
         adapter = new ArrayAdapter<Mood>(this,
-                R.layout.mood_listview, moodList);
+              R.layout.mood_listview, moodList);
         oldMoodList.setAdapter(adapter);
     }
 }

bc14460e618e1f6229ad0e479628140e2cfbbe33
Pensk <pensk@hotmail.ca>
2017-03-12 13:46:55 -0600

    create mood methods

--- app/src/main/java/ca/ualberta/andromeda/MoodController.java
+++ app/src/main/java/ca/ualberta/andromeda/MoodController.java
@@ -1,5 +1,6 @@
 package ca.ualberta.andromeda;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 /**
@@ -15,12 +16,26 @@ public class MoodController {
         userModel = ModelManager.getUserModel();
     }
 
-    public void addMood(Mood mood){
-        moodModel.addItem(mood);
+    public Mood createMood(User user, String socialSituation, Date date) {
+        Mood newMood = new Mood(user, socialSituation, date);
+        this.addMood(newMood);
+    }
+
+    public Mood createMood(User user, String socialSituation){
+        Mood newMood = new Mood(user, socialSituation, new Date());
+        this.addMood(newMood);
+    }
+
+    public ArrayList<Mood> getUserMoods(User user){
+        
     }
 
     public Mood getMood(int index){
         return moodModel.getItem(index);
+    }
+
+    public void addMood(Mood mood){
+        moodModel.addItem(mood);
     }
 
     public void updateMood(int index, User user, String situation, Date date){
@@ -29,5 +44,9 @@ public class MoodController {
 
     public void deleteMood(int index){
         moodModel.deleteItem(index);
+    }
+
+    public void deleteMood(Mood mood){
+        moodModel.deleteItem(mood);
     }
 }

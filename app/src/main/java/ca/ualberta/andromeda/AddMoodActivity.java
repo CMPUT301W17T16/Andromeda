764d154ea02c2344ba629516b8cbb37b3ab4f80e
brianzbw <brian.zbw@gmail.com>
2017-03-13 01:40:36 -0600

    get the location in addMood, not yet save to mood

--- app/src/main/java/ca/ualberta/andromeda/AddMoodActivity.java
+++ app/src/main/java/ca/ualberta/andromeda/AddMoodActivity.java
@@ -1,15 +1,27 @@
 package ca.ualberta.andromeda;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -26,14 +38,13 @@ public class AddMoodActivity extends AndromedaActivity{
     private MoodController moodController;
     private UserController userController;
     private Emotion.State state;
-
-
+    private LocationManager locationManager;
+    private LocationListener listener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         moodController = ModelManager.getMoodController();
         userController = ModelManager.getUserController();
-
         setContentView(R.layout.activity_add_mood);
         Button DeleteButton = (Button) findViewById(R.id.DeleteButton);
         Button SaveButton = (Button) findViewById(R.id.SaveButton);
@@ -44,6 +55,45 @@ public class AddMoodActivity extends AndromedaActivity{
         Spinner SocialSpinner = (Spinner) findViewById(R.id.SocialSitSpinner);
         EditText TriggerHolder = (EditText) findViewById(R.id.TriggerHolder);
         EditText DetailHolder = (EditText) findViewById(R.id.DetailHolder);
+
+        //get loction
+        Switch location = (Switch) findViewById(R.id.LocationSwitch);
+        location.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (isChecked){
+                    Toast.makeText(getApplicationContext(),"save Location", Toast.LENGTH_SHORT).show();
+                    locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+                    listener = new LocationListener() {
+                        @Override
+                        public void onLocationChanged(Location location) {
+                            Toast.makeText(getApplicationContext(),"\n " + location.getLongitude() + " " + location.getLatitude(), Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                        }
+
+                        @Override
+                        public void onProviderEnabled(String provider) {
+
+                        }
+
+                        @Override
+                        public void onProviderDisabled(String provider) {
+                            Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                            startActivity(i);
+                        }
+                    };
+                    configure_button();
+                }else {
+                    Toast.makeText(getApplicationContext(),"Don't save Location", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         // Loads the username
         Intent intent = getIntent();
@@ -141,7 +191,37 @@ public class AddMoodActivity extends AndromedaActivity{
             }
         });}
 
-        // Add location
+    private void configure_button() {
+        // first check for permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                                Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET}
+                        , 10);
+            }
+            return;
+        }
+        // this code won'textView execute IF permissions are not allowed, because in the line above there is return statement.
+
+        locationManager.requestLocationUpdates("gps", 5000, 0, listener);
+
+    }
+
+    //get permission to use location for this app
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 10:
+                configure_button();
+                break;
+            default:
+                break;
+        }
+    }
+
 
         // Add Image
 

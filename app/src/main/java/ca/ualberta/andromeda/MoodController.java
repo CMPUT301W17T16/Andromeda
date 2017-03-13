b2793b33edd299b745646807e148cadfdbe8db65
bgarbitt <bgarbitt@ualberta.ca>
2017-03-13 14:23:19 -0600

    Fixed updates

--- app/src/main/java/ca/ualberta/andromeda/MoodController.java
+++ app/src/main/java/ca/ualberta/andromeda/MoodController.java
@@ -59,7 +59,8 @@ public class MoodController {
     }
 
     public void updateMood(int index, String user, String situation, Date date, Emotion.State state, String trigger, String detail){
-        moodModel.getItem(index).edit("adfgh", situation, date, state, trigger, detail);
+        moodModel.getItem(index).edit(user, situation, date, state, trigger, detail);
+        moodModel.saveList();
     }
 
     public void deleteMood(int index){

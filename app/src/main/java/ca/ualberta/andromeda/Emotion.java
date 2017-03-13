312faad8006ad1be004440dd501c4b0f399df6ff
Pensk <pensk@hotmail.ca>
2017-03-13 11:36:10 -0600

    emotional states to string

--- app/src/main/java/ca/ualberta/andromeda/Emotion.java
+++ app/src/main/java/ca/ualberta/andromeda/Emotion.java
@@ -17,12 +17,31 @@ public class Emotion {
         this.state = emotion;
     }
 
-    public State getEmotion() {
-        return state;
-    }
-
     public void setEmotion(State emotion) {
         this.state = emotion;
+    }
+
+    public String getState() {
+        switch(state) {
+            case ANGER:
+                return "Anger";
+            case CONFUSION:
+                return "Confusion";
+            case DISGUST:
+                return "Disgust";
+            case FEAR:
+                return "Fear";
+            case HAPPINESS:
+                return "Happiness";
+            case SADNESS:
+                return "Sadness";
+            case SHAME:
+                return "Shame";
+            case SURPRISE:
+                return "Surprise";
+            default:
+                return "";
+        }
     }
 
     public String getColor() {
